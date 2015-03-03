5ea9b1c6c869f1d4ce5f692547b5c88c65d1dcb1
semproms-64 <danubio64@outlook.com>
2015-03-03 15:30:31 +0100

    loop_0.c

---
+++ Codes_C/loop_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define N 30000
+
+int main(int argc, char const *argv[])
+{
+	static int array[N][N];
+	int i, j;
+
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			array[j][i] = i*4;
+		}
+	}
+
+	return 0;
+}
