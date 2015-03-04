28866ef4b8c7307fe7b4b0343c571789ae407d0f
semproms-64 <danubio64@outlook.com>
2015-03-04 21:44:26 +0100

    space.c updated

--- Codes_C/space.c
+++ Codes_C/space.c
@@ -6,5 +6,5 @@ int main()
 	static int array[N][N];
 	printf("Size of C int: %d bytes\n", sizeof(int));
 	printf("Size of matrix with 20000 elements: %d bytes\n",sizeof(array));
-
+	printf("Size of C long: %d bytes\n", sizeof(long));
 }
