617615fa0be56da37710ec6bd6f2ff2eec1332d6
semproms-64 <danubio64@outlook.com>
2015-03-04 21:47:02 +0100

    space.c updated

--- Codes_C/space.c
+++ Codes_C/space.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #define N 20000
 
-int main()
+int main(int argc, char const *argv[])
 {
 	static int array[N][N];
 	printf("Size of C int: %d bytes\n", sizeof(int));
 	printf("Size of matrix with 20000 elements: %d bytes\n",sizeof(array));
 	printf("Size of C long: %d bytes\n", sizeof(long));
+	return 0;
 }
+
