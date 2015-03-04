1f7d6f63dae33069b8524c2656a9f5a3bf24561d
semproms-64 <danubio64@outlook.com>
2015-03-04 21:52:47 +0100

    space.c updated

--- Codes_C/space.c
+++ Codes_C/space.c
@@ -7,6 +7,11 @@ int main(int argc, char const *argv[])
 	printf("Size of C int: %d bytes\n", sizeof(int));
 	printf("Size of matrix with 20000 elements: %d bytes\n",sizeof(array));
 	printf("Size of C long: %d bytes\n", sizeof(long));
+	printf("Size of C long long: %d bytes\n", sizeof(long long));
+	printf("Size of C char: %d bytes\n", sizeof(char));
+	printf("Size of C float: %d bytes\n", sizeof(float));
+	printf("Size of C double: %d bytes\n", sizeof(double));
+	printf("Size of C short: %d bytes\n", sizeof(short));
 	return 0;
 }
 
