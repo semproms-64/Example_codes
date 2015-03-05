8300378fff1ca43c327ada3c01c1c40cfb8f8c12
semproms-64 <danubio64@outlook.com>
2015-03-05 22:56:13 +0100

    Changes list.py

--- Codes_Python/list.py
+++ Codes_Python/list.py
@@ -1,8 +1,13 @@
 l1 = [1,2,3,4,5] #A single list
-print l1
+print l1 #We print the list
 l2 = ['caballeros','mesa','cuadrada'] #Another list (boring)
-l3 = l1 + l2
-print l3
-print l3[5]
-print len(l1)
-print len(l2)
+l3 = l1 + l2 #Join the two lists
+print l3 #Print the result
+print l3[5] #We print the 5 element of the list
+print len(l1) #We print the length of l1
+print len(l2) #We print the length of l2
+print l1 #We print l1
+del l1[1] #Deleting the 1 element of l1
+print l1 #We print l1
+l1.append(512) #We add an element
+print l1 #Printing l1
