09dc1c478570f20be27cace910fee67569c58def
semproms-64 <danubio64@outlook.com>
2015-03-07 21:02:36 +0100

    Python threads added

---
+++ Codes_Python/Threads/thread_0.py
@@ -0,0 +1,19 @@
+import threading
+
+def worker():
+	print 'Probando_worker_1'
+	return
+
+def worker_2():
+	print 'Probando_worker_2'
+	return
+
+threads = list()
+
+for i in range(3):
+	t = threading.Thread(target=worker)
+	p = threading.Thread(target=worker_2)
+	threads.append(p)
+	threads.append(t)
+	t.start()
+	p.start()
