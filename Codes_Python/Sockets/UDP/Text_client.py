c558f69911091383f3a6cd89d686adf655a96b95
semproms-64 <danubio64@outlook.com>
2015-03-09 23:57:44 +0100

    Updates Python chat

--- Codes_Python/Sockets/UDP/Text_client.py
+++ Codes_Python/Sockets/UDP/Text_client.py
@@ -2,12 +2,24 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+
+IP_direction = raw_input('IP: ')
 name = raw_input('Select a name: ')
 
+def main():
+	try:
+		while True:
+			print name+': ',
+			x = raw_input()
+			if x.strip() == 'exit':
+				sock.sendto(name+' has gone',(IP_direction,8881))
+				break
+			else:
+				sock.sendto(x,(IP_direction,8881))
+	finally:
+		sock.close()
 
-try:
-	while True:
-		x = raw_input(name+': ')
-		sock.sendto(x,('localhost',8881))
-finally:
-	sock.close()
+if __name__ == "__main__":
+    main()
+
+
--- Codes_Python/Sockets/UDP/Text_server.py
+++ Codes_Python/Sockets/UDP/Text_server.py
@@ -3,11 +3,15 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('',8881))
 
-try:
-	while True:
-		print 'Server ready...' 
-		data, address = sock.recvfrom(8192)
-		print 'Datagram received from ', address
-		print 'Data received: ', data
-finally:
-	sock.close()
+def main():
+	try:
+		while True:
+			print 'Server ready...' 
+			data, address = sock.recvfrom(8192)
+			print 'Datagram received from ', address
+			print 'Data received: ', data
+	finally:
+		sock.close()
+
+if __name__ == "__main__":
+    main()
