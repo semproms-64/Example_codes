fa99f902d00bca9c112fa68cf819e9a0aec3c9ae
semproms-64 <danubio64@outlook.com>
2015-03-27 14:26:39 +0100

    In.java, Whitelist.java and StaticSETofInts.java

---
+++ Codes_Java/Algorithms/Whitelist.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Whitelist
+{
+	public static void main(String[] args) 
+	{
+		int[] w = In.readInts(args[0]);
+		StaticSETofInts set = new StaticSETofInts(w);
+		while(!StdIn.isEmpty())
+		{
+			int key = StdIn.readInt();
+			if (!set.contains(key)) 
+			{
+				StdOut.println(key);
+			}
+		}
+	}
+}
