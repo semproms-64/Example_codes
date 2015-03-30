72f85d2a3490a3ca5ed97165dac09468add9a461
semproms-64 <danubio64@outlook.com>
2015-03-30 16:13:31 +0200

    New median.c

--- Codes_C/Math/median.c
+++ Codes_C/Math/median.c
@@ -19,6 +19,43 @@ int isEven(int num)
 		return 0;
 }
 
+void quicksort(float[], int, int);
+int partition(float[], int, int);
+
+void quicksort(float a[], int l, int r)
+{
+	int j;
+
+	if (l < r)
+	{
+		j = partition(a, l, r);
+		quicksort(a, l, j-1);
+		quicksort(a, j+1, r);
+	}
+}
+
+int partition(float a[], int l, int r)
+{
+	int pivot, i, j, t;
+	pivot = a[l];
+	i = l;
+	j = r+1;
+
+	while(1)
+	{
+		do ++i; while(a[i] <= pivot && i <= r);
+		do --j; while(a[j] > pivot);
+		if (i >= j) break;
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+	t = a[l];
+	a[l] = a[j];
+	a[j] = t;
+	return j;
+}
+
  int main(int argc, char const *argv[])
  {
  	if (argc < 2)
@@ -26,18 +63,27 @@ int isEven(int num)
  		printf("Wrong number of arguments\n");
  		return 1;
  	}
-
  	int muestra = argc-1;
- 	int median;
+	float array[muestra];
+
+	int z;
+	for (z = 0; z < muestra; z++)
+	{
+		array[z] = atof(argv[z+1]); //From argv to int array.
+	}
+
+	quicksort(array, 0, argc-2); //Sorting the array
+ 	
+ 	float median;
  	if (isEven(muestra)==0)
  	{
- 		median = atoi(argv[argc/2]);
- 		printf("Median: %d\n", median);
+ 		median = array[(muestra-1)/2];
+ 		printf("Median: %.2f\n", median);
  	}
  	else
  	{
- 		median = (atoi(argv[argc/2])+atoi((argv[argc/2+1])))/2;
- 		printf("Median: %d\n", median);
+ 		median = (array[(muestra-1)/2] + array[((muestra-1)/2)+1])/2;
+ 		printf("Median: %.2f\n", median);
  	}
  	return 0;
  }
