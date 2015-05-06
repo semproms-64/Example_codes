eecb42f85a0f62cb48d6d75fa26c64a65c290fc3
semproms-64 <danubio64@outlook.com>
2015-05-06 10:45:41 +0200

    Alias again and again

--- Shell_scripts/alias_bash.sh
+++ Shell_scripts/alias_bash.sh
@@ -5,4 +5,4 @@ echo "alias actualizar='sudo apt-get update && sudo apt-get upgrade'" >> .bashrc
 echo "alias instalar='sudo apt-get install'" >> .bashrc
 echo "alias desinstalar='sudo apt-get remove'" >> .bashrc
 . ~/.bashrc
-echo "The work is done!"
+echo "The work is done, `whoami`!" 
