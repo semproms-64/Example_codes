519c78f795c63fad71b7bb7830c7d4dc4ebe9357
semproms-64 <danubio64@outlook.com>
2015-05-08 00:20:02 +0200

    New alias_bash

--- Shell_scripts/alias_bash.sh
+++ Shell_scripts/alias_bash.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Autor: Semproms
 cd
-echo "alias actualizar='sudo apt-get update && sudo apt-get upgrade'" >> .bashrc
-echo "alias instalar='sudo apt-get install'" >> .bashrc
-echo "alias desinstalar='sudo apt-get remove'" >> .bashrc
+echo "alias actualizar='sudo apt-get -y update && sudo apt-get -y upgrade'" >> .bashrc
+echo "alias instalar='sudo apt-get -y install'" >> .bashrc
+echo "alias desinstalar='sudo apt-get -y remove'" >> .bashrc
 . ~/.bashrc
 echo "The work is done, `whoami`!" 
