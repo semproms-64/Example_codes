196184963607897e09be33738012d274b1e96b2c
semproms-64 <danubio64@outlook.com>
2015-05-08 22:58:42 +0200

    install_apps_boot

--- Shell_scripts/install_apps_boot.sh
+++ Shell_scripts/install_apps_boot.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-#Autor: Semproms
+#Author: Semproms
 
 if [[ $UID != 0 ]]; 
 	then
-		echo "You are not sudo"
+		echo "You are not sudo."
 		exec sudo $0
 fi
-	
+
+echo "Hi, we're installing software. Please, `whoami`,  wait..."
+
+apt-get -y update
+apt-get -y upgrade
 apt-get -y install vlc
 apt-get -y install gimp
 apt-get -y install ttf-mscorefonts-installer
 apt-get -y install ubuntu-restricted-addons
+apt-get -y install firefox
+apt-get -y install libreoffice
+apt-get -y install myspell-es
+apt-get -y install openssh-server
+apt-get -y install openssh-client
+
+echo "The work is done, `whoami`"
+
+
+
+
