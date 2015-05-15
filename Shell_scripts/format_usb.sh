c64aaccf68f601d3959b4ae4c825df5369f84b70
semproms-64 <danubio64@outlook.com>
2015-05-15 10:58:15 +0200

    Added * to $var1

--- Shell_scripts/format_usb.sh
+++ Shell_scripts/format_usb.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+#Author: Semproms
 
 if [[ $UID != 0 ]]; 
 	then
 		exec sudo $0
 fi
 
+
 echo "I need an USB path"
 df
 read var1
-
-umount $var1
+umount $var1*
 
 echo "I need the format"
 echo "1.- NTFS"
@@ -26,3 +27,15 @@ case $var2 in
 	*) echo "Not in range"
 	;;
 esac
+
+#echo "Dou you want to change the name of the device (y/n)?"
+#read name_var
+
+#if [[ "$name_var" == "y" ]]; then
+	#echo "I need a new name"
+	#read new_name
+	#echo mtools_skip_check=1 >> ~/.mtoolsrc
+	#mlabel -i /dev/sdb4 ::semproms
+#fi
+
+
