3341918e4233db3689fbc1cbce60620be6f14b88
semproms-64 <danubio64@outlook.com>
2015-07-24 15:31:41 +0200

    New EDAs

--- Programing_challenges/queue.c
+++ Programing_challenges/queue.c
@@ -1,41 +1,34 @@
-typedef struct Queue
-{
+#include <stdio.h>
+
+typedef struct Queue {
         int capacity;
         int size;
         int front;
         int rear;
         int *elements;
 }Queue;
-/* crateQueue function takes argument the maximum number of elements the Queue can hold, creates
-   a Queue according to it and returns a pointer to the Queue. */
-Queue * createQueue(int maxElements)
-{
-        /* Create a Queue */
+
+Queue * createQueue(int maxElements) {
         Queue *Q;
         Q = (Queue *)malloc(sizeof(Queue));
-        /* Initialise its properties */
         Q->elements = (int *)malloc(sizeof(int)*maxElements);
         Q->size = 0;
         Q->capacity = maxElements;
         Q->front = 0;
         Q->rear = -1;
-        /* Return the pointer */
         return Q;
 }
-void Dequeue(Queue *Q)
-{
-        /* If Queue size is zero then it is empty. So we cannot pop */
+
+void Dequeue(Queue *Q) {
         if(Q->size==0)
         {
                 printf("Queue is Empty\n");
                 return;
         }
-        /* Removing an element is equivalent to incrementing index of front by one */
         else
         {
                 Q->size--;
                 Q->front++;
-                /* As we fill elements in circular fashion */
                 if(Q->front==Q->capacity)
                 {
                         Q->front=0;
@@ -43,19 +36,18 @@ void Dequeue(Queue *Q)
         }
         return;
 }
-int front(Queue *Q)
-{
+
+int front(Queue *Q) {
         if(Q->size==0)
         {
                 printf("Queue is Empty\n");
                 exit(0);
         }
-        /* Return the element which is at the front*/
         return Q->elements[Q->front];
 }
-void Enqueue(Queue *Q,int element)
-{
-        /* If the Queue is full, we cannot push an element into it as there is no space for it.*/
+
+void Enqueue(Queue *Q,int element) {
+    
         if(Q->size == Q->capacity)
         {
                 printf("Queue is Full\n");
@@ -64,18 +56,16 @@ void Enqueue(Queue *Q,int element)
         {
                 Q->size++;
                 Q->rear = Q->rear + 1;
-                /* As we fill the queue in circular fashion */
                 if(Q->rear == Q->capacity)
                 {
                         Q->rear = 0;
                 }
-                /* Insert the element in its rear side */ 
                 Q->elements[Q->rear] = element;
         }
         return;
 }
-int main()
-{
+
+int main() {
         Queue *Q = createQueue(5);
         Enqueue(Q,1);
         Enqueue(Q,2);
